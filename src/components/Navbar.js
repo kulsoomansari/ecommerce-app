7d17d7d0e2815195d0fb7a3d2c446453ca15dc64
Md-Faizan-Siddiqui <siddiquimdfaizan82@gmail.com>
2020-12-23 12:29:00 +0500

    add product form

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from "../images/ecom.svg";
 
 export default function Navbar() {
     return (
-        <div>
-            Navbar
+        <div className='navbox'>
+           <div className='leftside'>
+               <img src={logo} alt='' />
+          </div> 
+           <div className='rightside'>
+               <Link to='/signup' className='navlinks'>SIGNUP</Link>
+               <Link to='/login' className='navlinks'>LOGIN</Link>
+           </div> 
         </div>
     )
 }
