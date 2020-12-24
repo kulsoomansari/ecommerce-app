6cafa2fc1b59b4e790348f3e45315786e6ff0248
kulsoomansari <kulsoomansari412@gmail.com>
2020-12-23 19:15:17 -0800

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,9 @@ import {Link} from 'react-router-dom'
 export default function Navbar() {
     return (
         <div className='navbox'>
-           <div className='leftSide'></div>
+           <div className='leftSide'>
+               <img src={logo} alt=" "/>
+           </div>
          <div className='rightSide'>
              <Link to='/signup' className='navlink'>SIGNUP</Link>
              <Link to='/login' className='navlink'>LOGIN</Link>
