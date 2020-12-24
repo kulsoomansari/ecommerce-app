824b1218abf1946a27988d5565136c9a992e4953
kulsoomansari <kulsoomansari412@gmail.com>
2020-12-23 22:20:56 -0800

    add product page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from "../images/ecom.svg";
-
+import logo from '../images/ecom.svg'
 export default function Navbar() {
     return (
         <div className='navbox'>
-           <div className='leftSide'>
-               <img src={logo} alt=" "/>
+           <div className="leftside">
+           <img src={logo} alt='' />
            </div>
-         <div className='rightSide'>
-             <Link to='/signup' className='navlink'>SIGNUP</Link>
-             <Link to='/login' className='navlink'>LOGIN</Link>
-            </div>
+           <div className="rightside">
+               <Link to="/signup" className="navlinks">SIGNUP</Link>
+               <Link to="/login" className="navlinks">LOGIN</Link>
+           </div>
          </div>
     )
 }
